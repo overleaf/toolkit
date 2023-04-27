7924adfd7938d9c4c13ed3dbced33f0e1c20715e
Eric Mc Sween <5454374+emcsween@users.noreply.github.com>
2023-04-27 16:51:40 -0400

    Start Mongo in a replica set by default
    Starting with Overleaf version 4, Mongo needs to be in a replica set. By
    default, the replica set is made of a single instance.

---
+++ lib/shared-functions.sh
@@ -0,0 +1,18 @@
+# shellcheck shell=bash
+# shellcheck disable=SC2034
+# shellcheck source-path=..
+
+function read_config() {
+  source "$TOOLKIT_ROOT/lib/default.rc"
+  # shellcheck source=/dev/null
+  source "$TOOLKIT_ROOT/config/overleaf.rc"
+}
+
+function read_image_version() {
+  IMAGE_VERSION="$(head -n 1 "$TOOLKIT_ROOT/config/version")"
+  if [[ ! "$IMAGE_VERSION" =~ ^([0-9]+)\.[0-9]+\.[0-9]+(-RC)?$ ]]; then
+    echo "ERROR: invalid version '${IMAGE_VERSION}'"
+    exit 1
+  fi
+  IMAGE_VERSION_MAJOR=${BASH_REMATCH[1]}
+}
