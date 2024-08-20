5bc0e603a7a1ba74448e05aa1036498e8fd41d0e
Jakob Ackermann <jakob.ackermann@overleaf.com>
2024-08-20 15:11:21 +0100

    Fix unquoting of variables
    - Fix trimming of matching quote (no star after \1 reference)
    - Only trim a single quote (turn any-match into optional match)

--- lib/shared-functions.sh
+++ lib/shared-functions.sh
@@ -180,11 +180,11 @@ function check_sharelatex_env_vars() {
 function read_variable() {
   local name=$1
   grep -E "^$name=" "$TOOLKIT_ROOT/config/variables.env" \
-  | sed -r "s/^$name=([\"']*)(.+)\1*\$/\2/"
+  | sed -r "s/^$name=([\"']?)(.+)\1\$/\2/"
 }
 
 function read_configuration() {
   local name=$1
   grep -E "^$name=" "$TOOLKIT_ROOT/config/overleaf.rc" \
-  | sed -r "s/^$name=([\"']*)(.+)\1*\$/\2/"
+  | sed -r "s/^$name=([\"']?)(.+)\1\$/\2/"
 }
